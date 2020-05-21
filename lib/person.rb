65484e5f328c9fde6762a20adbe2f9f9ed44eda9
Toni  Diep <adiep123@gmail.com>
2020-05-21 16:50:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,5 +7,12 @@ class Person
     @eye_color = eye_color
     @height = height
     @weight = weight
+    @handed = handed
+    @complexion = complexion
+    @t_shirt_size = t_shirt_size
+    @wrist_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
+    @pant_width = @pant_width
   end
 end
